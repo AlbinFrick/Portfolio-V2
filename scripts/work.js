c9cd4cd88073faf476be0342646ef739611094c9
Albin Frick <albin@Albins-MBP.lan>
2020-11-16 03:55:09 +0100

    Adds content and functionallity to workpage

--- scripts/work.js
+++ scripts/work.js
@@ -5,3 +5,19 @@ const navbar = document.querySelector('#hamburgerMenu');
 menuButton.addEventListener('click', () => {
     navbar.classList.toggle('active');
 })
+
+const workCards = document.getElementsByClassName('work');
+const workContainer = document.getElementsByClassName('workContainer');
+for (let i = 0; i < workCards.length; i++) {
+    workCards[i].addEventListener('click', () => {
+        const loadedCards = document.getElementsByClassName('work');
+        for (var j = 0; j < loadedCards.length; j++) {
+            if (loadedCards[j].className.includes('active') && !workCards[i].className.includes('active')) {
+                loadedCards[j].classList.remove('active');
+                workContainer[0].classList.remove('active');
+            }
+        }
+        workCards[i].classList.toggle('active');
+        workContainer[0].classList.toggle('active');
+    })
+};
